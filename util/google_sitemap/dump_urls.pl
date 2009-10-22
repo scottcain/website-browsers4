728d63e49a1473bbf8ec0ad39a56557425eef99a
Todd Harris <todd@wormbase.org>
2009-10-22 10:46:58 -0400

    dumped get in favor of direct URLs; google does not like redirects

--- util/google_sitemap/dump_urls.pl
+++ util/google_sitemap/dump_urls.pl
@@ -7,26 +7,32 @@ $|++;
 my $path = shift or warn "Usage: dump_urls.pl [/path/to/release] (now using aceserver for testing)";
 
 my $db;
-if ($path) {
-  $db = Ace->connect(-path => $path);
-} else {
-  $db = Ace->connect(-host => 'localhost',-port => 2005);
-}
+#if ($path) {
+#    $db = Ace->connect(-path => $path);
+#} else {
+    $db = Ace->connect(-host => 'localhost',-port => 2005);
+#}
 
 my $version = $db->status->{database}{version};
 open OUT,">url_lists/$version-urls.txt";
 
-use constant USE_GET    => 1;
 use constant ROOT_URL   => 'http://www.wormbase.org/db';
+use constant USE_GET    => 0;
 use constant GET        => ROOT_URL . '/get?name=%s;class=%s';
 use constant ATTRIBUTES => 'changefreq=monthly';
 my %counts;
 my %classes = (
 #	      All_genes
-	      Anatomy_name    => { priority => '0.1' },
-	      Anatomy_term    => { priority => '0.3' },
-	      Antibody        => { priority => '0.3' },
-	      Author          => { priority => '0.3' },
+	      Anatomy_name    => { 
+		  url => ROOT_URL . '/ontology/anatomy?name=%s;class=%s',
+		  priority => '0.1' },
+	      Anatomy_term    => { 
+		  url => ROOT_URL . '/ontology/anatomy?name=%s;class=%s',
+		  priority => '0.3' },
+	      Antibody        => { 
+		  url => ROOT_URL . '/gene/antibody?name=%s;class=%s',
+		  priority => '0.3' },
+#	      Author          => { priority => '0.3' },
 #	      briggsae_CDS
 #	      Briggsae_genomic
 #	      briggsae_pseudogenes
@@ -36,7 +42,9 @@ my %classes = (
 #	      CDS     => { priority => '0.4' },
 #	      Cell    => { priority => '0.5' },
 #	      Cell_group => { priority => '0.1' },
-	      Clone   => { priority => '0.4' },
+	      Clone   => {
+		  url => ROOT_URL . '/seq/clone?name=%s;class=%s',
+		  priority => '0.4' },
 #	      Coding_transcripts
 #	      Condition
 #	      curated_CDS
@@ -46,8 +54,10 @@ my %classes = (
 #	      elegans_pseudogenes
 #	      elegans_RNA_genes
 #	      elegans_transposons
-	      Expression_cluster => { priority => '0.1' },
-	      Expr_pattern       => { priority => '0.3' },
+#	      Expression_cluster => { priority => '0.1' },
+	      Expr_pattern       => { 
+		  url => ROOT_URL . '/gene/expression?name=%s;class=%s',
+		  priority => '0.3' },
 #	      Expr_profile       => { priority => '0.1' },
 #	      Feature            => { priority => '0.1' },
 #	      Feature_data
@@ -57,56 +67,86 @@ my %classes = (
 	      },
 #	      Genetic_code
 #	      Genetic_map
-	      Gene_class  => { priority => '0.4' },
+	       Gene_class  => {
+		   url => ROOT_URL . '/gene/gene_class?name=%s;class=%s',
+		   priority => '0.4' },
 # Not necessary to index the *name classes - these objects point to the same URL
 # as the primary class (and the *name entry *should* appear on the page and be indexed
 #	      Gene_name   => { priority => '0.4' },
-	      Gene_regulation => { priority => '0.3' },
+	       Gene_regulation => {
+		   url => ROOT_URL . '/gene/regulation?name=%s;class=%s',
+		   priority => '0.3' },
 #	      Genome_Sequence
 #	      GO_code => { priority => '0.1' },
-	      GO_term => { priority => '0.3' },
+	      GO_term => { 
+		  url => ROOT_URL . '/gene/ontology?name=%s;class=%s',
+		  priority => '0.3' },
 #	      Grid
-	      Homology_group => { priority => '0.5' },
+	      Homology_group => {
+		  url => ROOT_URL . '/gene/homology_group?name=%s;class=%s',
+		  priority => '0.5' },
 #	      Homol_data
 #	      Insertion_allele
 #	      Journal
 #	      KeySet
 #	      KO_allele
-	      Laboratory => { priority => '0.3' },
-	      Life_stage => { priority => '0.3' },
+	      Laboratory => { 
+		  url => ROOT_URL . '/misc/laboratory?name=%s;class=%s',
+		  priority => '0.3' },
+	      Life_stage => {
+		  url => ROOT_URL . '/misc/life_stage?name=%s;class=%s',
+		  priority => '0.3' },
 #	      Lineage 
 #	      Live_genes
-	      Locus => { priority => '0.1' },
+#	      Locus => { priority => '0.1' },
 #	      Map
 #	      Method
 #	      Microarray => { priority => '0.1' },
 #	      Microarray_experiment => { priority => '0.1' },
 #	      Microarray_results => { priority => '0.1' },
 #	      Model
-	      Motif => { priority => '0.1' },
+	      Motif => {
+		  url => ROOT_URL . '/gene/motif?name=%s;class=%s',
+		  priority => '0.1' },
 #	      NBP_allele
 #	      NDB_Sequence
 #	      nematode_ESTs
 #	      Oligo       => { priority => '0.1' },
 #	      Oligo_set   => { priority => '0.1' },
-	      Operon      => { priority => '0.1' },
-	      Paper       => { priority => '0.5' },
-	      PCR_product => { priority => '0.1' },
-	      Person      => { priority => '0.3' },
-#	      Person_name => { priority => '0.3' },
-	      Phenotype   => { priority => '0.5' },
-#	      Protein     => {
-#		  url      => ROOT_URL . '/seq/protein?name=%s;class=%s',
-#		  priority => '0.7',
-#	      },	     	      
-	      Wormpep     => {
+	      Operon      => {
+		  url => ROOT_URL . '/gene/operon?name=%s;class=%s',
+		  priority => '0.1' },
+	      Paper       => {
+		  url => ROOT_URL . '/misc/paper?name=%s;class=%s',
+		  priority => '0.5' },
+	      PCR_product => {
+		  url => ROOT_URL . '/seq/pcr?name=%s;class=%s',
+		  priority => '0.1' },
+	       Person      => {
+		   url => ROOT_URL . '/misc/person?name=%s;class=%s',
+		   priority => '0.3' },
+	      Person_name => {
+		  url => ROOT_URL . '/misc/person?name=%s;class=%s',
+		  priority => '0.3' },
+	       Phenotype   => {
+		   url => ROOT_URL . '/misc/phenotype?name=%s;class=%s',
+		   priority => '0.5' },
+	      Protein     => {
 		  url      => ROOT_URL . '/seq/protein?name=%s;class=%s',
 		  priority => '0.7',
 	      },	     	      
+	       Wormpep     => {
+		   url      => ROOT_URL . '/seq/protein?name=%s;class=%s',
+		   priority => '0.7',
+	       },	     	      
 #	      Pseudogene
-	      Rearrangement => { priority => '0.1' },
+	      Rearrangement => {
+		  url => ROOT_URL . '/gene/rearrange?name=%s;class=%s',
+		  priority => '0.1' },
 #	      Restrict_enzyme
-	      RNAi => { priority => '0.4' },
+	      RNAi => {
+		  url => ROOT_URL . '/seq/rnai?name=%s;class=%s',
+		  priority => '0.4' },
 #	      Sage_tag => { priority => '0.1' },
 #	      Sequence => {
 #		  url      => ROOT_URL . '/seq/sequence?name=%s;class=%s',
@@ -115,21 +155,27 @@ my %classes = (
 #	      Sequence_map
 #	      SK_map
 #	      SO_term
-	      Strain => { priority => '0.4' },
+	      Strain => {
+		  url => ROOT_URL . '/gene/strain?name=%s;class=%s',
+		  priority => '0.4' },
 #	      Structure_data => { priority => '0.1' },
 #	      Substitution_allele
 #	      Tag
 #	      Transcript => { priority => '0.2' },
-	      Transgene  => { priority => '0.3' },
-	      Transposon => { priority => '0.1' },
-	      Transposon_family => { priority => '0.1' },
+	      Transgene  => {
+		  url => ROOT_URL . '/gene/transgene?name=%s;class=%s',
+		  priority => '0.3' },
+#	       Transposon => { priority => '0.1' },
+#	      Transposon_family => { priority => '0.1' },
 #	      Tree
-	      Variation => { priority => '0.6' },
+	      Variation => {
+		  url => ROOT_URL . '/gene/variation?name=%s;class=%s',
+		  priority => '0.6' },
 #	      View
 #	      Wormpep
 #	      worm_genes
 #	      Y2H => { priority => '0.1' },
-	      );
+	       );
 
 foreach (sort keys %classes) {
   fetch_features($_);
@@ -139,29 +185,30 @@ foreach (sort keys %classes) {
 
 
 sub fetch_features {
-  my $class = shift;
-  my @features;
+    my $class = shift;
+    my @features;
 #  if ($class eq 'Protein') {
 #      @features = $db->fetch(-query=>qq/find Protein where Species="C*elegans"/);
 #  } else {
-      @features = $db->fetch($class => '*');
+    @features = $db->fetch($class => '*');
 #  }
-  foreach my $feat (@features) {
+    foreach my $feat (@features) {
 #      next if ($class eq 'Protein' && $feat->Species ne 'Caenorhabditis elegans');
-      my $target = encode($feat);
-      $counts{$class}++;
-      
-    my ($url);
-    if (USE_GET) {
-      $url = GET;
-    } else {
-      $url = $classes{$class}{priority};
+	my $target = encode($feat);
+	$counts{$class}++;
+	
+	my $url;
+	if (USE_GET) {
+	    $url = GET;
+	} else {
+	    $url = $classes{$class}{url};
+	}
+	
+	# Append the priority if one has been set
+	my $priority = eval { $classes{$class}{priority} } || '0.1';
+	my $string = sprintf($url,$target,$class) . ' ' . join(' ',ATTRIBUTES,"priority=$priority") . "\n";
+	print OUT $string;
     }
-
-    my $priority = eval { $classes{$class}{priority} } || '0.1';
-    my $string = sprintf($url,$target,$class) . ' ' . join(' ',ATTRIBUTES,"priority=$priority") . "\n";
-    print OUT $string;
-  }
 }
 
 
@@ -173,5 +220,5 @@ sub encode {
 
 
 # update the symlink so the site map generator script can find it
-unlink("url_lists/current_urls.txt");
+unlink("current_urls.txt");
 symlink("url_lists/$version-urls.txt",'current_urls.txt');
