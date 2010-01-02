f85880450dcf51095283eb9619dcf9d8fb6a19a2
Todd Harris <todd@wormbase.org>
2010-01-02 13:49:32 -0500

    tweaks to the sitemap

--- util/google_sitemap/create_sitemap.sh
+++ util/google_sitemap/create_sitemap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ROOT=$PWD
-BIN=${PWD}/projects/wormbase/admin/util/google_sitemap
+BIN=/home/todd/projects/wormbase/admin/util/google_sitemap
 ${BIN}/dump_urls.pl /usr/local/acedb/elegans
 python ${BIN}/sitemap_gen/sitemap_gen.py --config=${BIN}/sitemap_gen/wormbase_config.xml
 /usr/local/bin/wget -q www.google.com/webmasters/sitemaps/ping?sitemap=http%3A%2F%2Fwww.wormbase.org%2Fsitemap_index.xml
