4531c23059ccaf760b6dfe2a730c1f3c0f0f6b0e
Todd Harris <bitbucket@tharris.org>
2010-05-18 17:11:52 -0400

    updated the site map for new structure of the website

--- util/google_sitemap/create_sitemap.sh
+++ util/google_sitemap/create_sitemap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BIN=/home/todd/projects/wormbase/admin/util/google_sitemap
-${BIN}/dump_urls.pl /usr/local/acedb/elegans
+BIN=/usr/local/wormbase/admin/util/google_sitemap
+${BIN}/dump_urls.pl /usr/local/wormbase/acedb/wormbase
 python ${BIN}/sitemap_gen/sitemap_gen.py --config=${BIN}/sitemap_gen/wormbase_config.xml
 /usr/local/bin/wget -q www.google.com/webmasters/sitemaps/ping?sitemap=http%3A%2F%2Fwww.wormbase.org%2Fsitemap_index.xml
