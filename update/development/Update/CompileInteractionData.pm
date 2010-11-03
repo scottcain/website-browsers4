0f0ca73aa1d76b2251fb045682a8ceed9d51ed39
norie <noriedlc3@gmail.com>
2010-11-03 12:14:33 -0400

    updated for full conformity to staging paradigm

--- update/development/Update/CompileInteractionData.pm
+++ update/development/Update/CompileInteractionData.pm
@@ -1,126 +1,138 @@
-package Update::CompileInteractionData; #
+package Update::CompileInteractionData;
 
-use base 'Update';
 use strict;
+use base 'Update';
 use Ace;
 
-our $support_db_dir;
-our $datadir;
-our $interaction_data_file;
+our $DB = Ace->connect(-host=>'localhost', -port=>2005);
 
+# The symbolic name of this step
 
-sub step {return 'compile interaction data';}
+sub step { return 'compiling interaction data'; }
 
-sub run{
+sub run {
 
-    my $self = shift @_;
-    my $release = $self->release;
-    
-    ### interaction dir should have been created
-    
-    $support_db_dir = $self->support_dbs;
-    $datadir = $support_db_dir."/$release/interaction";
-	$interaction_data_file = $datadir . "\/compiled_interaction_data.txt";
-	print "Compiling interaction data\n";
-	$self->compile_interaction_data();
+	my $self = shift;
+	my $release = $self->release;
+	
+	## directories
+	
+	my $support_db_dir = $self->support_dbs;	
+	my $datadir = $support_db_dir . "/$release/interaction";
+	my $outfile = $datadir . "/compiled_interaction_data.txt";
+
+	## subroutine call(s) 
+	
+	$self->compile_interaction_data($outfile);
 
 }
+
+
+
+## subroutines
 
 sub compile_interaction_data {
 
-my $class = 'Interaction';
-my $DB = Ace->connect(-host=>'localhost',-port=>2005);  # 'aceserver.cshl.org'
-
-my @interactions = $DB->fetch(-class=>$class);
-
-my @objects;
-
-open OUTFILE, ">$interaction_data_file" or die "Cannot open interaction data output file\n";
-
-foreach my $interaction (@interactions) {
-    #print "$interaction\n";
-    my $int_type = $interaction->Interaction_type;
-    if ($int_type =~ /predicted/i) {
+	my $self = shift @_;
+	my $outfile = shift @_;
 	
-	my $log_likelihood_score = $interaction->Log_likelihood_score;
+	open OUTFILE, ">$outfile" or die "Cannot open gene_rnai_pheno_data_compile output file\n"; 
 
-	if ($log_likelihood_score >= 1.5) {
-
-	    push @objects, $interaction;
-
-	} else {
-
-	    next;
-	}
-
-    } else {
-
-	push @objects, $interaction;
-    }
-
-
-} ## end foreach
-
-
-
-foreach my $interaction (@objects){  
-
-	eval {
-	# my $interaction = shift (@{$interaction_ref});
-	my $it = $interaction->Interaction_type;
-	my $type = $it;
-	my $rnai = $it->RNAi;
-	
-	my @non_directional_interactors;
-	my $effr;
-	my $effr_name;
-	my $effd;
-	my $effd_name;
-	my $interaction_vector;
-	
-	eval {
-		@non_directional_interactors = $it->Non_directional->col;
-	};
-	
-	if (@non_directional_interactors) {
-		$effr = shift @non_directional_interactors;
-	 	$effr_name = $effr->CGC_name;
-	if (!($effr_name)){
-		$effr_name = $effr->Sequence_name
-	}
-		$effd = shift @non_directional_interactors;
-		$effd_name = $effd->CGC_name;
-	if (!($effd_name)){
-		$effd_name = $effd->Sequence_name
-	}
-	
-	$interaction_vector = 'non_directional';
-	
-	}
-	else {
-		$effr = $it->Effector->right;
-	 	$effr_name = $effr->CGC_name;
-	if (!($effr_name)){
-		$effr_name = $effr->Sequence_name
-	}
-		$effd = $it->Effected->right;
-		$effd_name = $effd->CGC_name;
-	if (!($effd_name)){
-		$effd_name = $effd->Sequence_name
-	}
+	my $class = 'Interaction';
+	my $DB = Ace->connect(-host=>'localhost',-port=>2005);  # 'aceserver.cshl.org'
 	
 	
-	$interaction_vector = 'eftr\-\>eftd';
+	#my @interactions = $DB->fetch(-class=>$class, -count=>1000);
+	my @interactions = $DB->fetch(-class=>$class);
 	
-	}
+	my @objects;
 	
-	my $phenotype = $it->Interaction_phenotype;
-
-
-    print OUTFILE "$interaction\|$type\|$rnai\|$effr\|$effr_name\|$effd\|$effd_name\|$phenotype\|$interaction_vector\n";
-    }
+	
+	foreach my $interaction (@interactions) {
+		#print "$interaction\n";
+		my $int_type = $interaction->Interaction_type;
+		if ($int_type =~ /predicted/i) {
+		
+			my $log_likelihood_score = $interaction->Log_likelihood_score;
+		
+			if ($log_likelihood_score >= 1.5) {
+		
+				push @objects, $interaction;
+			} else {
+		
+				next;
+			}
+		} else {
+	
+			push @objects, $interaction;
+		}
+	
+	} ## end foreach my $interaction 
+	
+	
+	foreach my $interaction (@objects){  
+	
+		eval {
+		# my $interaction = shift (@{$interaction_ref});
+		my $it = $interaction->Interaction_type;
+		my $type = $it;
+		my $rnai = $it->RNAi;
+		
+		my @non_directional_interactors;
+		my $effr;
+		my $effr_name;
+		my $effd;
+		my $effd_name;
+		my $interaction_vector;
+		
+		eval {
+			@non_directional_interactors = $it->Non_directional->col;
+		};
+		
+		if (@non_directional_interactors) {
+			$effr = shift @non_directional_interactors;
+			$effr_name = $effr->CGC_name;
+		if (!($effr_name)){
+			$effr_name = $effr->Sequence_name
+		}
+			$effd = shift @non_directional_interactors;
+			$effd_name = $effd->CGC_name;
+		if (!($effd_name)){
+			$effd_name = $effd->Sequence_name
+		}
+		
+		$interaction_vector = 'non_directional';
+		
+		}
+		else {
+			$effr = $it->Effector->right;
+			$effr_name = $effr->CGC_name;
+		if (!($effr_name)){
+			$effr_name = $effr->Sequence_name
+		}
+			$effd = $it->Effected->right;
+			$effd_name = $effd->CGC_name;
+		if (!($effd_name)){
+			$effd_name = $effd->Sequence_name
+		}
+		
+		
+		$interaction_vector = 'eftr\-\>eftd';
+		
+		}
+		
+		my $phenotype = $it->Interaction_phenotype;
+	
+	
+		print OUTFILE "$interaction\|$type\|$rnai\|$effr\|$effr_name\|$effd\|$effd_name\|$phenotype\|$interaction_vector\n";
+		}
 }
 
+
+
+
+
 }
+
 
 1;
