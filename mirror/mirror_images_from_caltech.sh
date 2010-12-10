cc3b8c8e06be8290b27c1837ba8e693c3d049b7f
tharris <bitbucket@tharris.org>
2010-12-10 01:16:55 -0800

    fixed path so it is hopefully more general

--- mirror/mirror_images_from_caltech.sh
+++ mirror/mirror_images_from_caltech.sh
@@ -2,4 +2,5 @@
 
 # Grab images from Caltech once a night via rsync over ssh
 
-rsync --rsh=ssh -Cav /usr/local/wormbase/pictures/virtualworm/ tharris@wb-dev.oicr.on.ca:/usr/local/wormbase/website-shared-files/html/img/virtualworm
+#rsync --rsh=ssh -Cav /usr/local/wormbase/pictures/virtualworm/ tharris@wb-dev.oicr.on.ca:/usr/local/wormbase/website-shared-files/html/img/virtualworm
+rsync --rsh=ssh -Cav -L /usr/local/wormbase/pictures/ tharris@wb-dev.oicr.on.ca:/usr/local/wormbase/website-shared-files/html/img
