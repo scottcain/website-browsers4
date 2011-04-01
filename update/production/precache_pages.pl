d1d019ebf4c23ee65bb50f02ed241ca8af616714
Todd Harris <info@toddharris.net>
2011-04-01 11:58:24 -0400

    additional classes

--- update/production/precache_pages.pl
+++ update/production/precache_pages.pl
@@ -15,6 +15,11 @@ use WWW::Mechanize;
 $|++;
 
 use constant URL => 'http://dev.wormbase.org/db/gene/gene?class=Gene;name=';
+
+my %class2url = ( gene => 'http://dev.wormbase.org/db/gene/gene?class=Gene;name=',
+		  variation => 'http://dev.wormbase.org/db/gene/variation?class=Variation;name=',
+		  protein   => 'http://dev.wormbase.org/db/seq/protein?class=Protein;name=',);
+
 #use constant CACHE_ROOT => '/usr/local/wormbase/website/classic/html/cache';
 use constant CACHE_ROOT => '/usr/local/wormbase/databases/cache';
 
@@ -24,64 +29,75 @@ my $start = time();
 
 my $db    = Ace->connect(-host=>'localhost',-port=>2005);
 my $version = $db->status->{database}{version};
-my $cache = CACHE_ROOT . "/$version/gene";
-system("mkdir -p $cache");
 
-my $previous = shift;
-my %previous = parse(); # if $previous;
+my @classes = qw/gene protein variation/;
 
+
+foreach my $class (@classes) {
+    my $cache = CACHE_ROOT . "/$version/$class";
+    system("mkdir -p $cache");
+    
+    my $previous = shift;
+    my %previous = parse($cache); # if $previous;
+    
 #my $db    = Ace->connect(-path=>'/usr/local/acedb/elegans');
-
-
-open OUT,">>$cache/$version-precached-pages.txt";
-
-my %status;
+    
+    
+    open OUT,">>$cache/$version-precached-pages.txt";
+    
+    my %status;
 #my $i     = $db->fetch_many(-query=>qq{find Gene Species="Caenorhabditis elegans"});
 #my $i     = $db->fetch_many(-query=>qq{find Gene Species="Caenorhabditis elegans" AND CGC_name AND Molecular_name});
-my $i = $db->fetch_many('Gene','*');
-
-
-while (my $gene = $i->next) {
-    next unless $gene->Species eq 'Caenorhabditis elegans';
-   
-    $db ||= Ace->connect(-host=>'localhost',-port=>2005);
-
-
-    if ($previous{$gene}) {
-	print STDERR "Already seen gene $gene. Skipping...";
+    my $i = $db->fetch_many(ucfirst($class),'*');
+    
+    while (my $obj = $i->next) {
+	next unless $obj->Species eq 'Caenorhabditis elegans';
+	
+	$db ||= Ace->connect(-host=>'localhost',-port=>2005);
+	
+	
+	if ($previous{$obj}) {
+	    print STDERR "Already seen $class $obj. Skipping...";
+	    print STDERR -t STDOUT && !$ENV{EMACS} ? "\r" : "\n";
+	    next;
+	}
+	
+	print STDERR "Fetching and caching $obj\n";
 	print STDERR -t STDOUT && !$ENV{EMACS} ? "\r" : "\n";
-	next;
+	my $url = $class2url{$class} . $obj;
+	sleep 2;
+	
+	my $cache_start = time();
+	# No need to watch state - create a new agent for each gene to keep memory usage low.
+	my $mech = WWW::Mechanize->new(-agent => 'WormBase-PreCacher/1.0');
+	$mech->get($url);
+	my $success = ($mech->success) ? 'success' : 'failed';
+	my $cache_stop = time();
+	$status{$obj} = $success;
+	
+	if ($mech->success) {
+	    open CACHE,">$cache/$obj.html";
+	    print CACHE $mech->content;
+	    close CACHE;
+	}
+	
+	print OUT join("\t"
+		       ,$obj
+		       ,$class eq 'gene' ? $obj->Public_name : ''
+		       ,$url
+		       ,$success
+		       ,$cache_stop - $cache_start)
+	    ,"\n";
     }
-
-    print STDERR "Fetching and caching $gene\n";
-    print STDERR -t STDOUT && !$ENV{EMACS} ? "\r" : "\n";
-    my $url = URL . $gene;
-    sleep 2;
     
-    my $cache_start = time();
-    # No need to watch state - create a new agent for each gene to keep memory usage low.
-    my $mech = WWW::Mechanize->new(-agent => 'WormBase-PreCacher/1.0');
-    $mech->get($url);
-    my $success = ($mech->success) ? 'success' : 'failed';
-    my $cache_stop = time();
-    $status{$gene} = $success;
-    
-    if ($mech->success) {
-	open CACHE,">$cache/$gene.html";
-	print CACHE $mech->content;
-	close CACHE;
-    }
-    
-    print OUT join("\t",$gene,$gene->Public_name || '',$url,$success,$cache_stop - $cache_start),"\n";
+    my $end = time();
+    my $seconds = $end - $start;
+    print OUT "\n\nTime required to cache " . (scalar keys %status) . "objects: ";
+    printf OUT "%d days, %d hours, %d minutes and %d seconds\n",(gmtime $seconds)[7,2,1,0];
 }
 
-my $end = time();
-my $seconds = $end - $start;
-print OUT "\n\nTime required to cache " . (scalar keys %status) . "genes: ";
-printf OUT "%d days, %d hours, %d minutes and %d seconds\n",(gmtime $seconds)[7,2,1,0];
-
-
 sub parse {
+    my $cache = shift;
 #    open IN,"$previous";
 #    return unless (-e "$cache/$version-precached-pages.txt");
     if (-e "$cache/$version-precached-pages.txt") {
@@ -89,9 +105,9 @@ sub parse {
 	my %previous;
 	while (<IN>) {
 	    chomp;
-	    my ($gene,$name,$url,$status,$cache_stop) = split("\t");
-	    $previous{$gene}++ unless $status eq 'failed';
-	    print STDERR "Recording $gene as seen...";
+	    my ($obj,$name,$url,$status,$cache_stop) = split("\t");
+	    $previous{$obj}++ unless $status eq 'failed';
+	    print STDERR "Recording $obj as seen...";
 	    print STDERR -t STDOUT && !$ENV{EMACS} ? "\r" : "\n";
 	}
 	close IN;
