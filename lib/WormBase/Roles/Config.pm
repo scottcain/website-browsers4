d1d06807a229c916a3bc8953c7158ee4f4a33b61
Todd Harris <info@toddharris.net>
2011-09-05 14:19:59 -0400

    added backup script of wormbase_user database, to be run once a day

--- lib/WormBase/Roles/Config.pm
+++ lib/WormBase/Roles/Config.pm
@@ -290,7 +290,8 @@ has 'local_app_nodes' => (
             wb-web2.oicr.on.ca
             wb-web3.oicr.on.ca
             wb-web4.oicr.on.ca
-            wb-mining.oicr.on.ca
+            wb-gb1.oicr.on.ca
+            wb-gb2.oicr.on.ca
             wb-web6.oicr.on.ca
             wb-web7.oicr.on.ca
 	    wb-web8.oicr.on.ca
@@ -298,6 +299,8 @@ has 'local_app_nodes' => (
             wb-web10.oicr.on.ca/],
     },
     );
+
+#            wb-mining.oicr.on.ca
 
 has 'remote_app_nodes' => (
     is => 'ro',
