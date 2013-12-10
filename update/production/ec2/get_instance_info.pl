94eacd18d5e988dfdaf519bc508e1c8f32a358de
Todd Harris <info@toddharris.net>
2013-12-10 09:00:45 -0700

    helper script to report on existing instances

--- update/production/ec2/get_instance_info.pl
+++ update/production/ec2/get_instance_info.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
+use FindBin qw/$Bin/;
+use lib "$Bin/../../../lib";
 use strict;
-use VM::EC2;
+use WormBase::Update::EC2;
 use Getopt::Long;
 
 my ($format,$help,$status);
@@ -9,7 +11,6 @@ GetOptions('format=s'     => \$format,
 	   'help=s'        => \$help,
 	   'status=s'      => \$status,
     );
-
 
 if ($help) {
     die <<END;
@@ -27,70 +28,13 @@ END
 
 }
 
-# Connect to EC2 ; access_key and secret_key provided by ENV
-my $ec2 = VM::EC2->new(-endpoint    => 'http://ec2.amazonaws.com',
-		       -print_error => 1);
+my $agent = WormBase::Update::EC2->new({release => 'ec2'});
 
-
-my @i;
+my $i;
 if (!$status) {
-    @i = $ec2->describe_instances();
-    print STDERR "Listing all currently running instances...\n\n";
+    $i = $agent->get_instances();
 } else {
-    @i = $ec2->describe_instances({'tag:Status' => $status });
-    print STDERR "The following $status instances currently exist...\n\n";
+    $i = $agent->get_instances($status);
 }
 
-
-
-foreach my $i (@i) {
-    
-    my $id         = $i->instanceId; 
-    my $type       = $i->instanceType;
-    my $state      = $i->instanceState;
-    my $status     = $i->current_status;
-    my $zone       = $i->availabilityZone;
-    my $launched   = $i->launchTime;
-    my @groups     = $i->groups;
-    my $tags       = $i->tags;
-
-    # Network information
-    my $hostname   = $i->dnsName;
-    my $private_ip = $i->privateIpAddress;
-    my $public_ip  = $i->ipAddress;
-    
-    # EBS volumes
-    # my $block_dev  = $i->blockDeviceMapping;
-
-    # Pepper our environment
-    system('export WBSERVER$c="' . $hostname . '"');
-
-    if ($format eq 'short') {
-	print "  $hostname ($tags->{Role}; $state)\n";
-    } else {
-	print "  Instance: $id ($hostname)\n";
-	print "\tprivate ip address: $private_ip\n";
-	print "\t public ip address: $public_ip\n";
-	print "\t    instance type : $type\n";
-	print "\t             zone : $zone\n";
-	print "\t            state : $state\n";
-	print "\t           status : $status\n";
-	print "\t              TAGS\n";
-	foreach (sort keys %$tags) { 
-	    print "\t                    $_ : $tags->{$_}\n";
-	}
-	
-	print "\t              EBS mounts\n";
-	my @devices = $i->blockDeviceMapping;
-	foreach my $d (@devices) {
-	    my $virtual_device = $d->deviceName;
-	    print "\t                    $d : $virtual_device\n";
-	}
-	print "\n";
-    }
-}
-
-print "\n";	
-    
-
-
+$agent->display_instance_metadata($i);
